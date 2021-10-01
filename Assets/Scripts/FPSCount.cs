244523b1b9ca35efc9e8bbacce0b385e323ae2f5
adequatejack <72620456+adequatejack@users.noreply.github.com>
2021-10-01 17:04:52 +1000

    Add files via upload

---
+++ Assets/Scripts/FPSCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCount : MonoBehaviour
+{
+    public float count;
+    private Text txt;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+        count = 0f;
+        float exp = 60f;
+        txt.text = exp.ToString();
+        StartCoroutine(countT());
+    }
+    void Update()
+    {
+        count++;
+    }
+    public IEnumerator countT()
+    {
+        yield return new WaitForSeconds(1f);
+        txt.text = count.ToString();
+        count = 0f;
+        StartCoroutine(countT());
+    }
+}
